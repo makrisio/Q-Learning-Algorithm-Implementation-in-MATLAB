407b3f011004381c8647ddee21d26ed3e20bcf40
Ioannis Makris <ioannis@makris.com>
2015-03-27 20:11:36 +0000

    Updated graphs to output two separate graphs. Optimised the output and code.
    Signed-off-by: Ioannis Makris <ioannis@makris.com>

--- ReinforcementLearningGreedy.m
+++ ReinforcementLearningGreedy.m
@@ -4,7 +4,7 @@ function q=ReinforcementLearningGreedy(R, gamma, goalState, alpha, epsilon)
 % (http://people.revoledu.com/kardi/)
 %
 % Code amended by Ioannis Makris and Andrew Chalikiopoulos
-% Model for an agent to find shortest path through a 5x5 maze grid
+% Model for an agent to find shortest path through a 10x10 maze grid
 % This algorithm uses a the ?-greedy algorithm to choose the next state
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 clc;
@@ -29,20 +29,19 @@ q=zeros(size(R));        % initialize Q as zero
 q1=ones(size(R))*inf;    % initialize previous Q as big number
 count=0;                 % counter
 steps=0;                 % counts the number of steps to goal
-episodes=0;              % counts the number of episodes
 B=[];                    % matrix to add results of steps and episode count
 cumReward=0;             % counter to calculate accumulated reward
 exploitCount=0;
 exploreCount=0;
 
-for episode=0:50000
+for episode=1:50000
     
     state=5;        % Starting state of the agent
     
     
     while state~=goalState            % loop until find goal state
         % select any action from this state using ?-greedy
-        x=find(R(state,:)>=-50)         % find possible action of this state including punishment
+        x=find(R(state,:)>=-50);         % find possible action of this state including punishment
         if size(x,1)>0,
             
             r=rand; % get a uniform random number between 0-1
@@ -60,11 +59,11 @@ for episode=0:50000
          
          cumReward=cumReward+q(state,x1); %keep track of cumulative reward for graph
          exploitCount=exploitCount+1;
-         display('exploit');
+         %display('The agent exploits.');
          
      else        % explore
              x1=RandomPermutation(x);   % randomize the possible action
-             x1=x1(1)                  % select an action (only the first element of random sequence)
+             x1=x1(1);                  % select an action (only the first element of random sequence)
              
              if epsilon>=0.5
                 epsilon=epsilon*0.99999; % decrease epsilon
@@ -74,14 +73,14 @@ for episode=0:50000
              
              cumReward=cumReward+q(state,x1); %keep track of cumulative reward for graph
              exploreCount=exploreCount+1;
-             display('explore');
+             %display('The agent explores.');
      
      end
 
         x2 = find(R(x1,:)>=0);   % find possible steps from next step
         qMax=(max(q(x1,x2(1:end)))); % extract qmax from all possible next states
         q(state,x1)= q(state,x1)+alpha*((R(state,x1)+gamma*qMax)-q(state,x1));    % Temporal Difference Error
-        state=x1    % set state to next state
+        state=x1;    % set state to next state
         
         end
            
@@ -90,12 +89,9 @@ for episode=0:50000
             
         else
             steps=steps+1;
-            episodes=episodes+1; % if goal reach increase episode counter
-            A=[episodes; steps; cumReward];   % create episodes, steps and cumReward matrix
-            B=horzcat(B, A);    % add the new results to combined matrix
-            steps=0;    % reset steps counter to 0
-            cumReward=0;    % reset cumReward counter to 0
-            
+            %episodes=episodes+1; % if goal reach increase episode counter
+            A=[episode; steps; cumReward];   % create episodes, steps and cumReward matrix
+            B=horzcat(B, A);    % add the new results to combined matrix 
         end
     
     end
@@ -104,7 +100,7 @@ for episode=0:50000
     if sum(sum(abs(q1-q)))<0.00001 && sum(sum(q >0)) && epsilon<0.01
         if count>1000,
             q1=q;
-            episode  % report last episode
+            %episode  % report last episode
             break % for
         else
             q1=q;
@@ -114,6 +110,9 @@ for episode=0:50000
         q1=q;
         count=0;  % reset counter when deviation of q from previous q is large
     end
+    fprintf('Episode %i completed. The agent required %i steps to reach the goal.The cumulative reward gained is %i.\n', episode, steps, cumReward);
+    steps=0;    % reset steps counter to 0
+    cumReward=0;    % reset cumReward counter to 0
 end
 
 % row 4 in matrix is cumReward/steps taken per episode
@@ -125,18 +124,26 @@ B(4,:) = rdivide(B(3,:),B(2,:));
 
 %create a plot of episodes vs steps taken and episodes vs cumReward taken averaged against steps taken
 figure % new figure
-[hAx] = plotyy(B(1,1 : 5 : end),B(2,1 : 5 : end), B(1,1 : 5 : end),B(4,1 : 5 : end));
+[combinedGraph] = plotyy(B(1,1 : 5 : end),B(2,1 : 5 : end), B(1,1 : 5 : end),B(4,1 : 5 : end));
 
 title('Q-Learning Performance')
 xlabel('Episodes')
-ylabel(hAx(1),'Steps') % left y-axis
-ylabel(hAx(2),'Cumulative Reward/Steps') % right y-axis
+ylabel(combinedGraph(1),'Steps') % left y-axis
+ylabel(combinedGraph(2),'Cumulative Reward/Steps') % right y-axis
 
 % create a plot of episodes vs cumReward
-%plot(B(1,1 : 50 : end),B(3,1 : 50 : end));
+figure
+plot(B(1,1 : 5 : end),B(3,1 : 5 : end));
+title('Cumulative Rewards vs Episodes')
+xlabel('Episode')
+ylabel('Cumulative Reward')
 
 % create a plot of episodes vs steps
-%plot(B(1,:),B(2,:));
+figure
+plot(B(1, 1 : 5 : end),B(2, 1 : 5 : end));
+title('Steps vs Episodes')
+xlabel('Episode')
+ylabel('Steps')
 
 %normalize q
 g=max(max(q));
